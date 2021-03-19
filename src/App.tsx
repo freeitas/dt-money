3e3dd488b5d410bf77c0d3053d74c5a3e425e360
Guilherme Freitas <hfdsguilherme@gmail.com>
2021-03-18 21:15:39 -0300

    chore: modal form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./compoents/Header";
 import { Dashboard } from "./compoents/Dashboard";
+import { useState } from "react";
+import Modal from "react-modal";
+import { NewTransactionModal } from "./compoents/NewTransactionModal";
+
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
